5d05fe725cd7ad9095db4a8710e915f7c77a515f
Prantik Sanki <prantiksanki@gmail.com>
2025-05-29 16:25:59 +0530

    File upload

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express") ; 
+const app = express() ; 
+const PORT = 80 ; 
+
+
+app.get()
+
+
+
+app.listen(PORT, (req,res) =>
+{
+    console.log(`Server started ap port ${PORT}`) ; 
+}) ; 
+
